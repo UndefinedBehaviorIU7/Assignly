de34da3da2acbf76ee3e50b92d74fa364b378bf6
Binar <yardemyd@gmail.com>
2024-12-08 21:11:15 +0300

    Login screen added

--- app/src/main/java/com/example/assignly/presentation/Login.kt
+++ app/src/main/java/com/example/assignly/presentation/Login.kt
@@ -1,9 +1,171 @@
 package com.example.assignly.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.assignly.R
+
 
 @Composable
-fun Login() {
+fun Login(vm: LoginViewModel = viewModel()) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column (horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                stringResource(R.string.app_name),
+                fontSize = 40.sp,
+                modifier = Modifier.padding()
+            )
+            when (val uiState = vm.uiState.collectAsState().value) {
+                is LoginUiState.Idle -> {
+                    Column(
+                        modifier = Modifier.padding(top = 80.dp)
+                    ) {
+                        OutlinedTextField(
+                            value = uiState.login,
+                            onValueChange = { vm.loginChange(it) },
+                            label = { Text(stringResource(R.string.login)) },
+                            colors = OutlinedTextFieldDefaults.colors(
+                                focusedBorderColor = colorResource(R.color.active)
+                            ),
+                            modifier = Modifier.padding(bottom = 10.dp)
+                        )
+
+                        OutlinedTextField(
+                            value = uiState.password,
+                            onValueChange = { vm.passwordChange(it) },
+                            label = { Text(stringResource(R.string.password)) },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                            colors = OutlinedTextFieldDefaults.colors(
+                                focusedBorderColor = colorResource(R.color.active)
+                            ),
+                            modifier = Modifier.padding(bottom = 80.dp)
+                        )
+
+                        Button(
+                            onClick = { vm.auth() },
+                            contentPadding = PaddingValues(
+                                top = 10.dp,
+                                bottom = 10.dp,
+                                start = 20.dp,
+                                end = 20.dp
+                            ),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = colorResource(R.color.active)
+                            ),
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        ) {
+                            Text (
+                                text = stringResource(R.string.login),
+                                fontSize = 25.sp
+                            )
+                        }
+                    }
+                }
+
+                is LoginUiState.Loading -> {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+
+                is LoginUiState.Success -> {
+
+                    // Навигация
+                    Text(uiState.successMessage)
+                }
+
+                is LoginUiState.Error -> {
+                    Column (modifier = Modifier.padding(top = 80.dp)) {
+                        OutlinedTextField(
+                            value = uiState.login,
+                            onValueChange = { vm.loginChange(it) },
+                            label = { Text(stringResource(R.string.login)) },
+                            colors = OutlinedTextFieldDefaults.colors(
+                                focusedBorderColor = colorResource(R.color.active),
+                                unfocusedBorderColor = Color.Red
+                            ),
+                            modifier = Modifier.padding(bottom = 10.dp)
+                        )
+
+                        OutlinedTextField(
+                            value = uiState.password,
+                            onValueChange = { vm.passwordChange(it) },
+                            label = { Text(stringResource(R.string.password)) },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                            colors = OutlinedTextFieldDefaults.colors(
+                                focusedBorderColor = colorResource(R.color.active),
+                                unfocusedBorderColor = Color.Red
+                            ),
+                            modifier = Modifier.padding(bottom = 40.dp)
+                        )
+
+                        Text(
+                            text = "Error: ${uiState.errorMessage}",
+                            color = Color.Red,
+                            modifier = Modifier.padding(bottom = 40.dp).align(Alignment.CenterHorizontally),
+                        )
+
+                        Button(
+                            onClick = { vm.auth() },
+                            contentPadding = PaddingValues(
+                                top = 10.dp,
+                                bottom = 10.dp,
+                                start = 20.dp,
+                                end = 20.dp
+                            ),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = colorResource(R.color.active)
+                            ),
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        ) {
+                            Text (
+                                text = stringResource(R.string.login),
+                                fontSize = 25.sp
+                            )
+                        }
+                    }
+                }
+            }
+
+            TextButton(
+                onClick = {},
+                modifier = Modifier.padding(top = 7.dp)
+            ) {
+                Text(
+                    stringResource(R.string.signup),
+                    color = colorResource(R.color.active),
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/assignly/presentation/LoginViewModel.kt
+++ app/src/main/java/com/example/assignly/presentation/LoginViewModel.kt
@@ -1,7 +1,8 @@
 package com.example.assignly.presentation
 
-import android.health.connect.datatypes.HeartRateRecord
-import androidx.compose.runtime.mutableStateOf
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.assignly.api.NetworkService
@@ -22,6 +23,8 @@ sealed class LoginUiState {
     ): LoginUiState()
 
     data class Error (
+        val login: String,
+        val password: String,
         val errorMessage: String
     ): LoginUiState()
 
@@ -32,10 +35,11 @@ sealed class LoginUiState {
     ): LoginUiState()
 }
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel(application: Application): AndroidViewModel(application) {
     private val _uiState = MutableStateFlow<LoginUiState>(LoginUiState.Idle())
+    val uiState = _uiState.asStateFlow()
 
-    fun usernameChange(newLogin: String) {
+    fun loginChange(newLogin: String) {
         when (val current = _uiState.value) {
             is LoginUiState.Idle -> {
                 _uiState.value = current.copy(login = newLogin)
@@ -43,8 +47,8 @@ class LoginViewModel: ViewModel() {
 
             is LoginUiState.Error -> {
                 _uiState.value = LoginUiState.Idle (
-                    login = "",
-                    password = ""
+                    login = current.login,
+                    password = current.password
                 )
             }
 
@@ -69,13 +73,15 @@ class LoginViewModel: ViewModel() {
         }
     }
 
-    fun login() {
+    fun auth() {
         val current = _uiState.value
 
         if (current is LoginUiState.Idle) {
             if (current.login.isBlank() || current.password.isBlank()) {
                 _uiState.value = LoginUiState.Error (
-                    errorMessage = "Поля не должны быть пустыми"
+                    login = current.login,
+                    password = current.password,
+                    errorMessage = "Fields shouldn't be blank"
                 )
             }
 
@@ -93,23 +99,38 @@ class LoginViewModel: ViewModel() {
                     val id = request.id
                     val token = request.token
 
+                    val sharedPref = getApplication<Application>()
+                        .getSharedPreferences("auth", Context.MODE_PRIVATE)
+                    sharedPref.edit()
+                        .putString("token", token)
+                        .putInt("id", id)
+                        .apply()
+
                     _uiState.value = LoginUiState.Success (
                         id = id,
                         token = token,
-                        successMessage = "Вход успешно выполнен"
+                        successMessage = "Success"
                     )
+
+
                 } catch (e: HttpException) {
                     if (e.code() == 400) {
                         _uiState.value = LoginUiState.Error (
-                            errorMessage = "Неверный пароль"
+                            login = current.login,
+                            password = current.password,
+                            errorMessage = "incorrect password"
                         )
                     } else if (e.code() == 404) {
                         _uiState.value = LoginUiState.Error (
-                            errorMessage = "Пользователь не найден"
+                            login = current.login,
+                            password = current.password,
+                            errorMessage = "user not found"
                         )
                     } else {
                         _uiState.value = LoginUiState.Error (
-                            errorMessage = "Ошибка аутентификации"
+                            login = current.login,
+                            password = current.password,
+                            errorMessage = "authorisation error"
                         )
                     }
                 }
