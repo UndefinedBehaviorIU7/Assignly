cffaccb25cab63878c0fdbf847bf32911a858634
PS <jooonsnow@mail.ru>
2024-12-09 21:31:52 +0300

    gool

--- app/src/main/java/com/example/assignly/TaskPreview.kt
+++ app/src/main/java/com/example/assignly/TaskPreview.kt
@@ -1,7 +1,7 @@
 package com.example.assignly
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -9,23 +9,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,6 +42,7 @@ fun tasksList(taskCount: Int) {
             .fillMaxSize()
             .background(color = Color.LightGray),
         verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
         contentPadding = PaddingValues(10.dp)
     ) {
         items(taskCount) { index -> taskPreview(340) }
@@ -53,8 +52,16 @@ fun tasksList(taskCount: Int) {
 
 @Composable
 fun taskPreview(size: Int) {
-    Box (modifier = Modifier.background(Color.White).width(size.dp)){
-        Box (modifier = Modifier.align(Alignment.TopStart)) {
+    Box(
+        modifier = Modifier
+            .background(Color.White, shape = RoundedCornerShape(20.dp))
+            .width(size.dp),
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(10.dp)
+                .align(Alignment.TopStart)
+        ) {
             Column(
                 modifier = Modifier
                     .width((size * 2 / 3).dp)
@@ -72,7 +79,11 @@ fun taskPreview(size: Int) {
             }
         }
 
-        Box (modifier = Modifier.align(Alignment.TopEnd)) {
+        Box(
+            modifier = Modifier
+                .padding(10.dp)
+                .align(Alignment.TopEnd)
+        ) {
             Ring(
                 modifier = Modifier
                     .size(20.dp)
@@ -83,15 +94,21 @@ fun taskPreview(size: Int) {
         Box(modifier = Modifier.align(Alignment.BottomEnd)) {
             Card(
                 modifier = Modifier
+                    .padding(10.dp)
                     .border(1.dp, Color.Black, shape = RoundedCornerShape(10.dp))
                     .fillMaxHeight(),
                 shape = RoundedCornerShape(8.dp),
             ) {
-                Text(
-                    text = "22.08",
-                    modifier = Modifier
-                        .padding(5.dp)
-                )
+                Row()
+                {
+                    Image(painter = painterResource(R.drawable.calendar), contentDescription = "", Modifier.padding(5.dp))
+                    Text(
+                        text = "22.08",
+                        modifier = Modifier
+                            .padding(5.dp)
+                    )
+                }
+
             }
         }
     }
