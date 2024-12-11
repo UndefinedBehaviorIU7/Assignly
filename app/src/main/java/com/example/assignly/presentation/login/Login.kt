3f9367b825e3a0a4e843b723a84dfc25cfa89507
Binar <yardemyd@gmail.com>
2024-12-11 12:59:52 +0300

    Useless imports removed

--- app/src/main/java/com/example/assignly/presentation/login/Login.kt
+++ app/src/main/java/com/example/assignly/presentation/login/Login.kt
@@ -1,11 +1,7 @@
 package com.example.assignly.presentation.login
 
 import android.annotation.SuppressLint
-import android.graphics.ImageDecoder
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -27,9 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -39,7 +33,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import coil.compose.rememberAsyncImagePainter
 import com.example.assignly.R
 
 @SuppressLint("ResourceType")
