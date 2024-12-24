e184e9753c7ecb896267dbe621665bf3b774d3bd
Binar <yardemyd@gmail.com>
2024-12-24 17:21:35 +0300

    aboba

--- app/src/main/java/com/example/assignly/presentation/taskInfo/TaskInfoViewModel.kt
+++ app/src/main/java/com/example/assignly/presentation/taskInfo/TaskInfoViewModel.kt
@@ -1,7 +1,7 @@
 package com.example.assignly.presentation.taskInfo
 
 import android.app.Application
-import androidx.compose.runtime.collectAsState
+import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import coil.network.HttpException
@@ -11,16 +11,50 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class TaskInfoViewModel(application: Application): AndroidViewModel(application) {
-    private val _uiState = MutableStateFlow(TaskInfoUiState.Idle(Task()))
+class TaskInfoViewModel(application: Application, taskId: Int): AndroidViewModel(application) {
+    private val _uiState = MutableStateFlow<TaskInfoUiState>(TaskInfoUiState.Loading)
     val uiState = _uiState.asStateFlow()
+
+    private val id = taskId
+    private val sharedPref = getApplication<Application>()
+        .getSharedPreferences("auth", Context.MODE_PRIVATE)
+    val token = sharedPref.getString("token", "")
+
+    suspend fun fetchTasks(): Task? {
+        try {
+            val request = NetworkService.api.taskById(token = token.toString(), taskId = id)
+            return request
+        } catch (e: HttpException) {
+            return null
+        }
+    }
 
     init {
         viewModelScope.launch {
-            try {
-                val request = NetworkService.api.taskById()
+            val task = fetchTasks()
+            if (task != null) {
+                _uiState.value = TaskInfoUiState.Idle(
+                    task = task
+                )
+            } else {
+                _uiState.value = TaskInfoUiState.Error(
+                    errorMessage = "request error"
+                )
+            }
+        }
+    }
 
-            } catch (e: HttpException) {
+    fun retry() {
+        viewModelScope.launch {
+            val task = fetchTasks()
+            if (task != null) {
+                _uiState.value = TaskInfoUiState.Idle(
+                    task = task
+                )
+            } else {
+                _uiState.value = TaskInfoUiState.Error(
+                    errorMessage = "request error"
+                )
             }
         }
     }
